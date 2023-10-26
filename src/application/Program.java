352aea306c27ef2f6307263c778867ee260e1b07
Nedson Nogueira <nedson.junior91@gmail.com>
2023-10-25 22:47:14 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dpt = new Department(2, "Ned");
+		
+		System.out.println(dpt);
+		
+	}
+
+}
